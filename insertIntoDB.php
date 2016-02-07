3f0a43772491088a6f621f1f83a1040acbe9a8c8
adriantre <adrianto@stud.ntnu.no>
2016-02-07 15:06:53 +0100

    asd

--- insertIntoDB.php
+++ insertIntoDB.php
@@ -1,6 +1,6 @@
 <?php
 // Kalles med:
-// xmlhttp.open("GET","http://folk.ntnu.no/adrianto/insertIntoDB.php?table=" + gjeldende tabell + "&newValues=" + verdier som skal settes inn,true);
+// xmlhttp.open("POST","http://folk.ntnu.no/adrianto/insertIntoDB.php?table=" + gjeldende tabell + "&newValues=" + verdier som skal settes inn,true);
 $table = filter_input(INPUT_GET, "table");
 $newValues = filter_input(INPUT_GET, "newValues");
 echo $posX.",".$posY;
@@ -14,15 +14,17 @@ if ($conn === false) {
   exit;
 }
 
-$result = pg_insert($conn, $table, $newValues);
-if (!$result) {
-  echo "ERROR: Can not return values from DB.\n";
-  var_dump(pg_last_error($conn));
-  exit;
-}
+pg_insert($conn, $table, $newValues);
 
-while ($row = pg_fetch_row($result))
-{
-  echo $row[1] . "\n";
-}
+// $result = pg_insert($conn, $table, $newValues);
+// if (!$result) {
+//   echo "ERROR: Can not return values from DB.\n";
+//   var_dump(pg_last_error($conn));
+//   exit;
+// }
+
+// while ($row = pg_fetch_row($result))
+// {
+//   echo $row[1] . "\n";
+// }
 ?>
