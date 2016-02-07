bf26af9149e0cbbc80b683b7dff257f854bfd914
adriantre <adrianto@stud.ntnu.no>
2016-02-07 22:26:07 +0100

    asd

--- insertIntoDB.php
+++ insertIntoDB.php
@@ -1,28 +1,25 @@
 <?php
-// Kalles med:
-// xmlhttp.open("POST","http://folk.ntnu.no/adrianto/insertIntoDB.php?table=" + gjeldende tabell + "&newValues=" + verdier som skal settes inn,true);
-$table = filter_input(INPUT_GET, "table");
-$newValues = filter_input(INPUT_GET, "newValues");
-echo $posX.",".$posY;
+	$input = file_get_contents("php://input");
 
-
-$conn = pg_connect("host=92.62.34.78 port=5432 dbname=adrianto user=adrianto password=kalende");
-pg_set_error_verbosity($conn, PGSQL_ERRORS_VERBOSE);
-if ($conn === false) {
-  echo "ERROR: Can not connect to DB\n";
-  var_dump(pg_last_error($conn));
-  exit;
+	$conn = pg_connect("host=92.62.34.78 port=5432 dbname=adrianto user=adrianto password=kalende");
+	pg_set_error_verbosity($conn, PGSQL_ERRORS_VERBOSE);
+	
+	if ($conn === false) {
+		echo "An error occurred.\n";
+		var_dump(pg_last_error($conn));
+	exit;
 }
-
-$result = pg_insert($conn, $table, $newValues);
-if (!$result) {
-  echo "ERROR: Can not return values from DB.\n";
-  var_dump(pg_last_error($conn));
-  exit;
-}
-
-while ($row = pg_fetch_row($result))
-{
-  echo $row[1] . "\n";
-}
+ 
+  $json_object = json_encode($input);
+ 
+  $array = array();
+ 
+  $table = $json_object->table;
+  foreach ($json_object->data as $column)
+  {
+    $array[$column->column] = $column->data;
+  }
+ 
+  pg_insert($conn, $table, $array);
+  pg_query($conn, "select * from public.user;");
 ?>
