b5d4fba35dc8451ae7cad667cf5c60539d182265
adriantre <adrianto@stud.ntnu.no>
2016-02-15 15:59:15 +0100

    implementerte sqlopt

--- insertIntoDB.php
+++ insertIntoDB.php
@@ -3,17 +3,16 @@
 
 	$conn = pg_connect("host=92.62.34.78 port=5432 dbname=adrianto user=adrianto password=kalende");
 	pg_set_error_verbosity($conn, PGSQL_ERRORS_VERBOSE);
-	echo "Hei";
 	if ($conn === false) {
 		echo "An error occurred connecting to the database.\n";
 		var_dump(pg_last_error($conn));
 	exit;
 }
- 	echo "Hei";
 	$json_object = json_decode($input);
 
 	$array = array();
 
+	$sqlopt = $json_object->sqlopt;
 	$table = $json_object->table;
 	foreach ($json_object->data as $column)
 	{
@@ -22,5 +21,16 @@
 
 
 	echo implode("|",$array);
-	pg_insert($conn, $table, $array);
+	if ($sqlopt == "insert")
+	{
+		pg_insert($conn, $table, $array);
+	} else if ($sqlopt == "update")
+	{
+		pg_update($conn, $table, $array);
+	
+	} else if ($sqlopt == "delete")
+	{
+		pg_delete($conn, $table, $array);
+	}
+
 ?>
