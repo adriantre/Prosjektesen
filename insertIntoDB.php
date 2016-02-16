877655fac16151ea5db24658f116f40e469e49be
adriantre <adrianto@stud.ntnu.no>
2016-02-16 13:43:47 +0100

    s

--- insertIntoDB.php
+++ insertIntoDB.php
@@ -21,6 +21,16 @@
 	echo ($table);
 	echo implode("|",$array);
 	pg_query($conn, "SELECT * FROM pulic.user;")
+	if (!$result) {
+	  echo "An error asd occurred.\n";
+	  var_dump(pg_last_error($conn));
+	  exit;
+	}
+
+	while ($row = pg_fetch_row($result))
+	{
+	  echo $row[1] . "\n";
+	}
 	// pg_insert($conn, $table, $array);
 
 	// if ($sqlopt == "insert")
