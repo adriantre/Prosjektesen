63cc54843839f2fd907ac6dac3cf56ae4f9bb2be
adriantre <adrianto@stud.ntnu.no>
2016-02-18 14:50:10 +0100

    k

--- pushToDB.php
+++ pushToDB.php
@@ -19,16 +19,17 @@
 		$array[$column->column] = $column->data;
 	}
 
-    switch ($sqlopt) {
-    case 'insert':
-        $result = pg_insert($conn, $table, $array);
-        break;
-    case 'update':
-        $result = pg_update($conn, $table, $array);     
-        break;
-    case 'delete':
-        $result = pg_delete($conn, $table, $array);
-        break;
-    }
+	pg_insert($conn, $table, $array);
+    // switch ($sqlopt) {
+    // case 'insert':
+    //     $result = pg_insert($conn, $table, $array);
+    //     break;
+    // case 'update':
+    //     $result = pg_update($conn, $table, $array);     
+    //     break;
+    // case 'delete':
+    //     $result = pg_delete($conn, $table, $array);
+    //     break;
+    // }
 
 ?>
