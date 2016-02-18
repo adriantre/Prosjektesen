80295bd199e47a57ab871122ad284207f33413ea
adriantre <adrianto@stud.ntnu.no>
2016-02-18 14:02:09 +0100

    my

--- dbHead.js
+++ dbHead.js
@@ -1,11 +1,13 @@
-function createNewUser(sqlopt) {
+function createNewUser(operation) {
 
     var xmlhttp = new XMLHttpRequest();
     var url = 'http://folk.ntnu.no/adrianto/prosjektesen/pushToDB.php/';
 
+    var user_id;
     var user_name = document.getElementById("user_name").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("pwd").value;
+    var current_location_id;
     //validering
 
     xmlhttp.open('POST', url, true);
@@ -22,26 +24,96 @@ function createNewUser(sqlopt) {
         // Men dere trenger i utgangspunktet ikke å gjøre noe her.
       }
     }
- 
-    var user = {
-        'sqlopt': sqlopt,
-        'table': 'public.user',
-        'data': [
+    
+    switch(operation) {}
+        case 'newUser':
+        var user = {
+            'sqlopt': insert,
+            'table': 'public.user',
+            'data': [
+                {
+                    'column': 'user_name',
+                    'data': user_name
+                },
+                {
+                    'column': 'email',
+                    'data': email
+                },
+                {
+                    'column': 'password',
+                    'data': password
+                },
+            ]
+        };
+        var usertext = JSON.stringify(user);
+        xmlhttp.send(usertext);
+        break;
+    case 'getUser':
+        var user = {
+            'sqlopt': select,
+            'table': 'public.user',
+            'data': [
+                {
+                    'column': 'user_name',
+                    'data': user_name
+                },
+                {
+                    'column': 'password',
+                    'data': password
+                },
+            ]
+        };
+        var usertext = JSON.stringify(user);
+        xmlhttp.send(usertext);
+        //return user_id
+        break;
+    case 'deleteUser':
+        var user = {
+            'sqlopt': delete,
+            'table': 'public.user',
+            'data': [
+                {
+                    'column': 'user_id',
+                    'data': user_id
+                },
+            ]
+        };
+        var usertext = JSON.stringify(user);
+        xmlhttp.send(usertext);
+        //return user_id
+        break;
+    case 'updateUserLocation':
+                var user = {
+            'sqlopt': select,
+            'table': 'public.user',
+            'data': [
+                {
+                    'column': 'user_id',
+                    'data': user_id
+                },
+                {
+                    'column': 'current_location_id',
+                    'data': location
+                },
+            ]
+        };
+    }
+
+        switch (sqlopt) {
+        case 'insert':
+            $result = pg_insert($conn, $table, $array);
+            if (!$result)
             {
-                'column': 'user_name',
-                'data': user_name
-            },
-            {
-                'column': 'email',
-                'data': email
-            },
-            {
-                'column': 'password',
-                'data': password
-            },
-        ]
-    };
-    var usertext = JSON.stringify(user);
-    xmlhttp.send(usertext);
+                //brukeren finnes fra før eller liknende.
+            }
+            break;
+        
+        case 'update':
+            $result = pg_update($conn, $table, $array);     
+            break;
+        case 'delete':
+            $result = pg_delete($conn, $table, $array);
+    }
+
     // document.location.href = "http://folk.ntnu.no/adrianto/prosjektesen/mapPage.html";
 }
