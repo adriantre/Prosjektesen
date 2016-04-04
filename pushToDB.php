77e91afa12d55b9d97c496b402be07fd67f7ffa2
adriantre <adrianto@stud.ntnu.no>
2016-04-04 15:12:33 +0200

    ghjkghjkghj

--- pushToDB.php
+++ pushToDB.php
@@ -31,7 +31,7 @@
 	{
 		foreach ($json_object->conditions as $column)
 		{
-			$conditions[$column->column] = "'" . $column->data . "'";
+			$conditions[$column->column] = $column->data;
 		}
 	}
 	if (array_key_exists('to_select', $json_object))
