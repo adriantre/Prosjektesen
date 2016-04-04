97ecdd9b2908fc0e6e51088e600caf9a4cecc66d
LarsHenrik <larshbj@gmail.com>
2016-04-04 13:45:20 +0200

    dytte inn polygon

--- head/locationDbHead.js
+++ head/locationDbHead.js
@@ -1,4 +1,4 @@
-function manageLocation(operation, object) {
+function manageLocation(operation) {
 
     var xmlhttp = new XMLHttpRequest();
     var url = 'http://folk.ntnu.no/adrianto/prosjektesen/pushToDB.php/';
@@ -26,16 +26,16 @@ function manageLocation(operation, object) {
                 'values': [
                     {
                         'column': 'location_name',
-                        'data': object.getName()
+                        'data': location_name
                     },
                     {
-                        'column': 'geofence2s',
-                        'data': object.getGeofence()
+                        'column': 'geofence2',
+                        'data': 'ST_GeomFromText(' + geofence + ')'
                     },
-                    // {
-                    //     'column': 'creator_id',
-                    //     'data': creator_id
-                    // },
+                    {
+                        'column': 'creator_id',
+                        'data': 2
+                    },
                 ]
             };
             break;
--- mapPage_script.js
+++ mapPage_script.js
@@ -4,6 +4,7 @@ var map;
 var popup = L.popup();
 var layer;
 var wkt;
+var geofence;
 
 var drawControl;
 var drawnItems;
@@ -137,12 +138,12 @@ function cancelLocation() {
 function submitLocation() {
   map.removeLayer(this.layer);
   var location_name = document.getElementById("location_name").value;
-  var geofence = wkt.write();
+  geofence = wkt.write();
   document.getElementById('polygonCoords').innerHTML=geofence;
   $("#myModal").modal();
   // var newLocation = new Location(location_name, geofence);
-  localStorage.setItem("location_name", newLocation.getName());
-  manageLocation('newLocation', newLocation);
+  localStorage.setItem("location_name", location_name);
+  manageLocation('newLocation');
    // $("#success-alert").alert();
    // $("#success-alert").fadeTo(2000, 500).slideUp(500, function(){
    //    $("#success-alert").alert('close');
