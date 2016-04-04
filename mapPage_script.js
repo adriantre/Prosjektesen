323c5a9e23919e97ea1e12c59e24ffdebc0575f4
LarsHenrik <larshbj@gmail.com>
2016-04-04 15:59:00 +0200

    FIKSET PÅ MAIN MENU OG SCRIPTS

--- mapPage_script.js
+++ mapPage_script.js
@@ -18,47 +18,7 @@ var polygonDrawer;
 
 function initialize() {
 
-  // $(document).ready(function () {
-  var trigger = $('.hamburger'),
-      overlay = $('.overlay'),
-     isClosed = false;
-
-    trigger.click(function () {
-      hamburger_cross();      
-    });
-
-    // overlay.click(function () {
-    //   overlay_click();
-    // });
-
-    // function overlay_click() {
-    //   if(isClosed == true) {
-    //     overlay.hide();
-    //     trigger.removeClass('is-open');
-    //     trigger.addClass('is-closed');
-    //     isClosed = false;
-        
-    //   }
-    // }
-
-    function hamburger_cross() {
-
-      if (isClosed == true) {          
-        overlay.hide();
-        trigger.removeClass('is-open');
-        trigger.addClass('is-closed');
-        isClosed = false;
-      } else {   
-        overlay.show();
-        trigger.removeClass('is-closed');
-        trigger.addClass('is-open');
-        isClosed = true;
-      }
-  }
-  
-  $('[data-toggle="offcanvas"]').click(function () {
-        $('#wrapper').toggleClass('toggled');
-  });  
+    initSideMenu();
 // });
 
   // $("#success-alert").hide();
