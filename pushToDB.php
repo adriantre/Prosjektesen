e3886e86f057b26abcdacc46b94cb352a548a66a
adriantre <adrianto@stud.ntnu.no>
2016-04-04 13:52:05 +0200

    f

--- pushToDB.php
+++ pushToDB.php
@@ -61,11 +61,13 @@
 	        break;
     }
 
-	if (is_bool($result)) {
-		echo $result ? 'true' : 'false';
-		exit;
-	}
-    while ($result_row = pg_fetch_assoc($result)) {
-    	echo json_encode($result_row);
-    }
+    echo $sql;
+
+	// if (is_bool($result)) {
+	// 	echo $result ? 'true' : 'false';
+	// 	exit;
+	// }
+ //    while ($result_row = pg_fetch_assoc($result)) {
+ //    	echo json_encode($result_row);
+ //    }
 ?>
