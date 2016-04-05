2443dfde726ad9119db8ff91b71828b12324b4a2
adriantre <adrianto@stud.ntnu.no>
2016-04-05 12:17:54 +0200

    sd

--- pushToDB.php
+++ pushToDB.php
@@ -49,7 +49,7 @@
 	        pg_insert($conn, $table, $values);
 	        // pg_query($conn, "insert into " . $table . " values " . )
 	        $sql = "select currval('" . $table . "_" . $json_object->table . "_id_seq');";
-	        $result = pg_query($conn, $sql);
+	        // $result = pg_query($conn, $sql);
 	        break;
 	    case "update":
 	        $result = pg_update($conn, $table, $values, $conditions);     
