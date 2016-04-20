021c74e7cef27b2f761097c71094dbd5130756dd
adriantre <adrianto@stud.ntnu.no>
2016-04-20 11:24:09 +0200

    fetch_row

--- pushToDB.php
+++ pushToDB.php
@@ -78,7 +78,7 @@
 		exit;
 	}
 	if ($table == 'public.location_user' || $table == 'public.group_user') {
-		while ($result_row = pg_fetch_array($result, null, PGSQL_NUM) {
+		while ($result_row = pg_fetch_row($result, null) {
     		echo json_encode($result_row);
     	}
 	} else {
