fb37fb6f9d907cde50d404566f7d4c3c8732aa74
adriantre <adrianto@stud.ntnu.no>
2016-04-20 15:52:09 +0200

    asd

--- pushToDB.php
+++ pushToDB.php
@@ -84,7 +84,7 @@
     	    while ($result_row = pg_fetch_assoc($result)) {
     			$str .= json_encode($result_row) . ",";
     		}
-    		substr($str, 0, -1);
+    		$str = rtrim($str, ",");
     		$str .= "]";
     		echo $str;
     		exit;
