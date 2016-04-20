0f08f3cdf64b5712b0964625829c7b0df7c85cdf
adriantre <adrianto@stud.ntnu.no>
2016-04-20 11:42:50 +0200

    fant syntaxfail

--- pushToDB.php
+++ pushToDB.php
@@ -78,7 +78,7 @@
 		exit;
 	}
 	if ($table == 'public.location_user' || $table == 'public.group_user') {
-		while ($result_row = pg_fetch_row($result, null) {
+		while ($result_row = pg_fetch_row($result, null)) {
     		echo json_encode($result_row);
     	}
 	} else {
