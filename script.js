a09364d23995668be4cfcf8d2e8b7b4ad32a68ca
adriantre <adrianto@stud.ntnu.no>
2016-04-21 10:22:28 +0200

    modal newGroup

--- script.js
+++ script.js
@@ -86,6 +86,12 @@ function initializeMapPage() {
 });
 }
 
+function newGroup(){
+  $("#new_group_name").modal();
+  manageGroup('newGroup');
+
+}
+
 
 function getUserPosition(position) {
     coords =[];
