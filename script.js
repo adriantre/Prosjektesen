0b0b04de6442003b40a8b9b848ecb58a12b48ef3
LarsHenrik <larshbj@gmail.com>
2016-04-21 16:53:31 +0200

    lkmmklm

--- script.js
+++ script.js
@@ -213,23 +213,28 @@ function openMapPage() {
 var t1;
 var linebreak;
 var t2;
-function myInnFunction() {
-    document.getElementById("demo").innerHTML;
-    demo.className ="text";
-    t1 = document.createTextNode("Log");
-    demo.appendChild(t1);
-    linebreak = document.createElement('br');
-    demo.appendChild(linebreak);
-    t2 = document.createTextNode("out");
-    demo.appendChild(t2);
-}
-function myOutFunction(){
-    document.getElementById("demo").innerHTML;
-   demo.removeChild(t1);
-   demo.removeChild(t2);
-   demo.removeChild(linebreak);
-    demo.className ="glyphicon glyphicon-off";
-}
+// function myInnFunction() {
+//     document.getElementById("demo").innerHTML;
+//     demo.className ="text";
+//     t1 = document.createTextNode("Log");
+//     demo.appendChild(t1);
+//     linebreak = document.createElement('br');
+//     demo.appendChild(linebreak);
+//     t2 = document.createTextNode("out");
+//     demo.appendChild(t2);
+// }
+// function myOutFunction(){
+//     document.getElementById("demo").innerHTML;
+//    demo.removeChild(t1);
+//    demo.removeChild(t2);
+//    demo.removeChild(linebreak);
+//     demo.className ="glyphicon glyphicon-off";
+// }
+
+$('#myCarousel').carousel({
+  interval: false;
+});
+
 function showMyGroups(){
   manageGroup('getFriendLocations');
   var response = JSON.parse(localStorage.getItem("FriendLocations"));
